b7d2b0c6e5c26b58c9ff060a07c9f9a71c223c70
Alok Omkar <alokomkar.gudikote@gmail.com>
2018-09-04 17:33:01 +0530

    Cleanup

--- app/src/main/java/com/alokomkar/scrapbook/MainActivity.kt
+++ app/src/main/java/com/alokomkar/scrapbook/MainActivity.kt
@@ -14,8 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
