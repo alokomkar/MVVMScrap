912372b06c67be9c1ad2cb67f1d33011da75dbf6
alokomkar <alokomkar.gudikote@gmail.com>
2018-09-04 09:03:28 +0530

    Added reference urls

--- app/src/main/java/com/alokomkar/scrapbook/WordCountRepository.kt
+++ app/src/main/java/com/alokomkar/scrapbook/WordCountRepository.kt
@@ -6,6 +6,11 @@ import android.os.AsyncTask
 
 class WordCountRepository( application: Application ) : DataAPI {
 
+    //https://www.journaldev.com/20126/android-rss-feed-app
+    //jsoup parsing mvvm android
+    //https://github.com/hasancse91/android-web-scraping-app-jsoup/blob/master/WebScrapingbyJsoup-code/app/src/main/java/com/hellohasan/webscrapingbyjsoup/Parser/HtmlParser.java
+    //https://medium.com/@princessdharmy/getting-started-with-jsoup-in-android-594e89dc891f
+    //https://www.yudiz.com/data-scraping-in-android-using-jsoupjava-html-parser/
     override fun parseUrl(url: String) {
         WordCountTask(this ).executeOnExecutor( AsyncTask.THREAD_POOL_EXECUTOR )
     }
