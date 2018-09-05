31e5eef630771728091c8c11539ac198442a59e7
Alok Omkar <alokomkar.gudikote@gmail.com>
2018-09-05 16:33:45 +0530

    Added gradle files - test impl

--- app/src/test/java/com/alokomkar/scrapbook/WordCountViewModelTest.kt
+++ app/src/test/java/com/alokomkar/scrapbook/WordCountViewModelTest.kt
@@ -1,8 +1,31 @@
 package com.alokomkar.scrapbook
 
+import android.app.Application
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
 class WordCountViewModelTest {
 
     private lateinit var mWordCountViewModel : WordCountViewModel
 
+    @Mock private lateinit var mWordCountRepository: WordCountRepository
+    @Mock private lateinit var mApplication: Application
+
+    @Before fun setupWordCountViewModel() {
+        //Initialize mocks
+        MockitoAnnotations.initMocks( this )
+        //Initialize VM
+        mWordCountViewModel = Mockito.spy(WordCountViewModel(mApplication))
+    }
+
+    @Test
+    fun verifyUrlParse() {
+        mWordCountViewModel.loadTask("https://viacom18.com")
+
+
+    }
 
 }
